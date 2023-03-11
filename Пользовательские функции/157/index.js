e4ecd6214b2f915b4f9e24ff325309c788a02a9b
stasfilippov <kron888999@gmail.com>
2023-03-11 13:47:30 +0300

    add: параметры функции

---
+++ Пользовательские функции/157/index.js
@@ -0,0 +1,19 @@
+'use strict'; 
+
+function calcCub (n) {
+    console.log (n ** 3); 
+}
+
+calcCub (3); 
+
+//============================
+
+function checkNumb (numb) {
+    if (numb > 0) {
+        console.log ('+++')
+    } else {
+        console.log ('---');
+    }
+}
+
+checkNumb (11);
