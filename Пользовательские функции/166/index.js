d74a5c4a67465521020f540a2b520d712c52977f
stasfilippov <kron888999@gmail.com>
2023-03-12 15:08:55 +0300

    add: сокращенная форма if

---
+++ Пользовательские функции/166/index.js
@@ -0,0 +1,27 @@
+'use strict'; 
+
+function func(num1, num2) {
+	let res;
+	
+	if (num1 > 0 && num2 > 0) {
+		res = num1 * num2;
+	} else {
+		res = num1 - num2;
+	}
+	
+	return res;
+}
+
+console.log(func(3, 4));
+
+// сокращенная форма
+
+function func (num1, num2) {
+    if (num1 > 0 && num2 > 0) {
+        return num1 * num2;
+    } else {
+        return num1 - num2;
+    }
+}
+
+console.log(func(3, 4));
