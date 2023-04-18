9bda822452f6ac0aba07f4f6921b50377855c8ed
stasfilippov <kron888999@gmail.com>
2023-04-18 23:12:58 +0300

    add: мое решение - асинхр функция для загр картинок

--- Асинхронность/Асинхронный код с коллбэками/index.js
+++ Асинхронность/Асинхронный код с коллбэками/index.js
@@ -56,3 +56,39 @@ function make (numFirst, numSecond) {
 make (3, 4)
 //Задание --Сделайте так, чтобы функция make принимала два параметра: номер одного и другого элемента массива. Пусть результатом асинхронной операции эта функция возвращает сумму указанных элементов. 
 
+//Обработка исключений в коллбэках
+
+function make(num, callback) {
+	setTimeout(function() {
+		let arr = [1, 2, 3, 4, 5];
+		
+		let err;
+		if (arr[num] === undefined) {
+			err = 'elem not exists'; // текст ошибки
+		} else {
+			err = null; // ошибки нет
+		}
+		
+		callback(arr[num], err);
+	}, 3000);
+}
+
+// Функция загрузки картинки
+// Задание №3 Реализуйте функцию loadImage. Используйте для этого изученный вами ранее код для загрузки картинок.
+
+function loadImage (src, callback) {
+    let image = document.createElement('img');
+    image.src = src;
+
+    image.addEventListener('load', function() {
+        callback (image, err); // размещаем по загрузке
+    });
+}
+
+loadImage ('image.png', (image, err) => {
+    if (!err) {
+		document.body.append(image);
+	} else {
+		console.log('произошла ошибка: ' + err);
+	}
+})
