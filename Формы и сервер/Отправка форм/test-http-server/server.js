70248af8128ae8805817b107892fcdbd19350115
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-04-20 21:41:40 +0300

    add: отправка формой двух чисел

--- Формы и сервер/Отправка форм/test-http-server/server.js
+++ Формы и сервер/Отправка форм/test-http-server/server.js
@@ -1,6 +1,7 @@
 export default {
-	'/handler/': function(data) {
-		console.log(data);
+	'/handler/': function ({get}) { //получаем объект в get в параметр функции
+		console.log(get.num1);
+		console.log(get.num2);
 		return 'form data received';
 	}
 }
