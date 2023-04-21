1c82915b7155bdde478619bfdd41b83e6d2ee1b1
stasfilippov <kron888999@gmail.com>
2023-04-21 18:51:53 +0300

    add: querySelector

---
+++ Основы работы с DOM/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let button = document.querySelector('#button');
+console.log(button);
+
+//Задание 1
+//Запишите ссылку на каждый из абзацев в отдельную переменную и выведите содержимое каждой из этих переменных в консоль.
+
+let elem1 = document.querySelector('#elem1')
+console.log(elem1);
+
+let elem2 = document.querySelector('#elem2')
+console.log(elem2);
+
+let elem3 = document.querySelector('#elem3')
+console.log(elem3);
