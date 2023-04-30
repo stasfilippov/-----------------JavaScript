f0f427d27fa7803399e24a78ec9cebcf13a81d46
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-04-30 13:37:55 +0300

    add: EventListeners-for-btns

--- Основы работы с DOM/Bind-handler-to-elem/index.js
+++ Основы работы с DOM/Bind-handler-to-elem/index.js
@@ -1,7 +1,22 @@
 'use strict';
 
-let button = document.querySelector('#button');
+// let button = document.querySelector('#button');
 
-button.addEventListener('click', () => {
-	console.log('!!!')
+// button.addEventListener('click', () => {
+// 	console.log('!!!')
+// })
+
+
+let btns = document.querySelectorAll('input');
+
+btns[0].addEventListener('click', () => {
+	alert('1');
+})
+
+btns[1].addEventListener('click', () => {
+	alert('2');
+})
+
+btns[2].addEventListener('click', () => {
+	alert('3');
 })
