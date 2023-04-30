9033700d820fc647c4bbb1265fea2274679320e9
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-04-30 14:23:22 +0300

    add: one-handler-for-several-elem

--- Основы работы с DOM/named-event-handlers/index.js
+++ Основы работы с DOM/named-event-handlers/index.js
@@ -1,16 +1,34 @@
 'use strict';
 
-function func1 () {
-	console.log(1);
+// function func1 () {
+// 	console.log(1);
+// }
+
+// function func2 () {
+// 	console.log(2);
+// }
+
+// const btn1 = document.querySelector('#button1');
+// const btn2 = document.querySelector('#button2');
+
+// btn1.addEventListener('click', func1)
+// btn2.addEventListener('click', func2)
+
+
+function func() {
+	console.log('message');
 }
 
-function func2 () {
-	console.log(2);
-}
+const text1 = document.querySelector('#elem1')
+const text2 = document.querySelector('#elem2')
+const text3 = document.querySelector('#elem3')
+const text4 = document.querySelector('#elem4')
+const text5 = document.querySelector('#elem5')
 
-const btn1 = document.querySelector('#button1');
-const btn2 = document.querySelector('#button2');
 
-btn1.addEventListener('click', func1)
-btn2.addEventListener('click', func2)
+text1.addEventListener('click', func)
+text2.addEventListener('click', func)
+text3.addEventListener('click', func)
+text4.addEventListener('click', func)
+text5.addEventListener('click', func)
 
