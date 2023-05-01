b3a1688fb95a383b69e6420fc94fc9f9507d8f1e
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-05-01 21:46:34 +0300

    add: textContent

--- Основы работы с DOM/textContent/index.js
+++ Основы работы с DOM/textContent/index.js
@@ -1,11 +1,32 @@
 'use strict';
 
-//Дан абзац и кнопка. По клику на кнопку прочитайте текст абзаца и выведите его в консоль.
 const button = document.querySelector('#button');
-const elem = document.querySelector('#elem');
+const numb1 = document.querySelector('#num1');
+const numb2 = document.querySelector('#num2');
+const numb3 = document.querySelector('#num3');
+const result = document.querySelector('#result');
 
-button.addEventListener('click', () => {
+//Дан абзац и кнопка. По клику на кнопку прочитайте текст абзаца и выведите его в консоль.
+/* button.addEventListener('click', () => {
 	console.log(elem.textContent);
+}) */
+
+//Дан абзац и кнопка. По клику на кнопку запишите в абзац новый текст.
+/* button.addEventListener('click', () => {
+	console.log(elem.textContent = 'hello world')
+}) */
+
+//Даны два абзаца, содержащие своим текстом какие-то числа, и кнопка. По нажатию на кнопку выведите на экран сумму хранящихся чисел.
+// button.addEventListener('click', () => {
+// 	const sum = Number(numb1.textContent) + Number(numb2.textContent)
+
+// 	alert(sum);
+// })
+
+//Даны три абзаца с числами, див, и кнопка. По нажатию на кнопку запишите в текст дива сумму чисел.
+button.addEventListener('click', () => {
+	const res = Number(numb1.textContent) + Number(numb2.textContent) + Number(numb3.textContent);
+
+	result.textContent = res;
+
 })
-
-
