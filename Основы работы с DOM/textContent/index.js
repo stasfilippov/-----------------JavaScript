72cff7469989a5157bd8017b8a775018e1985598
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-05-01 22:08:47 +0300

    add: complete all task

--- Основы работы с DOM/textContent/index.js
+++ Основы работы с DOM/textContent/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const button = document.querySelector('#button');
-const numb1 = document.querySelector('#num1');
-const numb2 = document.querySelector('#num2');
-const numb3 = document.querySelector('#num3');
-const result = document.querySelector('#result');
+// const numb1 = document.querySelector('#num1');
+// const numb2 = document.querySelector('#num2');
+// const numb3 = document.querySelector('#num3');
+// const result = document.querySelector('#result');
+const elem = document.querySelector('.text');
 
 //Дан абзац и кнопка. По клику на кнопку прочитайте текст абзаца и выведите его в консоль.
 /* button.addEventListener('click', () => {
@@ -24,9 +25,23 @@ const result = document.querySelector('#result');
 // })
 
 //Даны три абзаца с числами, див, и кнопка. По нажатию на кнопку запишите в текст дива сумму чисел.
+// button.addEventListener('click', () => {
+// 	const res = Number(numb1.textContent) + Number(numb2.textContent) + Number(numb3.textContent);
+
+// 	result.textContent = res;
+
+// })
+
+//Дан абзац с числом и кнопка. По нажатию на кнопку прибавьте к значению абзаца единицу и запишите полученное число обратно
+// button.addEventListener('click', () => {
+// 	const res = Number(numb1.textContent) + 1;
+
+// 	numb1.textContent = res;
+
+// })
+
+//Дан абзац с текстом и кнопка. По нажатию на кнопку запишите в конец текста абзаца восклицательный знак.
 button.addEventListener('click', () => {
-	const res = Number(numb1.textContent) + Number(numb2.textContent) + Number(numb3.textContent);
-
-	result.textContent = res;
-
+	elem.textContent = `${elem.textContent}!`
+	
 })
