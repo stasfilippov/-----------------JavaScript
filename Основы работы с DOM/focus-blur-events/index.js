1e3e041ceeb49beaf5916a1df2ce10d0b95e2c51
stasfilippov <kron888999@gmail.com>
2023-05-02 23:25:30 +0300

    add: focus/blur events for input

---
+++ Основы работы с DOM/focus-blur-events/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const inputField = document.querySelector('#inputField')
+
+//#1
+// inputField.addEventListener('focus', () => {
+//     inputField.value = 1;
+// })
+
+// inputField.addEventListener('blur', () => {
+//     inputField.value = 2;
+// })
+
+//#2
+// inputField.addEventListener('blur', () => {
+//     inputField.value = Math.pow(inputField.value, 2)
+// })
+
+//#3
+// inputField.addEventListener('focus', () => {
+//     inputField.value = '';
+// })
