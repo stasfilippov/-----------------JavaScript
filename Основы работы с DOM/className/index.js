8238e20835a37b084b429510764d1c0590d776eb
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-05-06 14:36:36 +0300

    add: работа с className

---
+++ Основы работы с DOM/className/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const button = document.querySelector('#button')
+const elem = document.querySelector('#elem')
+
+//№1
+// button.addEventListener('click', () => {
+//     alert(elem.className)
+// })
+
+//№2
+// button.addEventListener('click', () => {
+//     elem.className = 'name'
+// })
+
+//#3
+// button.addEventListener('click', () => {
+//     const arr = [];
+
+//     for(let name of elem.classList) {
+//         arr.push(name);
+//     }
+
+//     console.log(arr)
+// })
